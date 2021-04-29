d7258f30d00157ccd317abf775d543a07505a2ea
bguessportfolio <guesb325@newschool.edu>
2021-04-28 20:41:04 -0400

    maybe world will work better than country??? idk?

--- capstone/capstone-site.js
+++ capstone/capstone-site.js
@@ -3,5 +3,5 @@ function exampleFunction() {
 	}
 gsap.to(".logo", {duration: 2, x: 400, backgroundColor: "#560563", borderRadius: "20%", border: "5px solid white", ease: "bounce"});
 var map = d3.geomap()
-	.geofile ('d3-geomap/topojson/countries/USA.json')
+	.geofile ('d3-geomap/topojson/world/countries.json')
 	.draw(d3.select('#map'));
