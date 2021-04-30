cbb8d6343ea1f1dc5217a7f2da3dc735ffdfc064
bguessportfolio <guesb325@newschool.edu>
2021-04-30 13:35:11 -0400

    syntax error pt2

--- capstone/capstone-site.js
+++ capstone/capstone-site.js
@@ -8,7 +8,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     maxZoom: 18,
     id: 'mapbox/light-v9',
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',,
+		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     tileSize: 512,
     zoomOffset: -1
 }).addTo(map);
