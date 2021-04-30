f08cdb00098d696b40ac0bcc2a046f17ec8517a1
bguessportfolio <guesb325@newschool.edu>
2021-04-30 13:30:47 -0400

    syntax error

--- capstone/capstone-site.js
+++ capstone/capstone-site.js
@@ -5,7 +5,7 @@ gsap.to(".logo", {duration: 2, x: 400, backgroundColor: "#560563", borderRadius:
 var map = L.map('map').setView([37.8, -96], 4);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
-    maxZoom: 18;
+    maxZoom: 18,
     id: 'mapbox/light-v9',
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
 		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',,
